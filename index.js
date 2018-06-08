8c07ec74437412b08d238cb0496c62055c35921c
Ben Hurst <BHurst8@gmail.com>
2018-06-08 15:40:45 -0700

    2nd try

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const app = express();
-const PORT = 8080;
+//const PORT = 8080;
 
 app.use(express.static('./public'));
 
-app.listen(PORT, () => {
-  console.log('Web Server up on port', PORT);
+app.listen(process.env.PORT, () => {
+  console.log('Web Server up on port', process.env.PORT);
 });
